e6e6b436f5f84c27d99ed2a3737fa4c04bf1bc80
tolltech <alexandrovpe@gmail.com>
2021-12-21 16:17:37 +0400

    21 tmp

---
+++ AoC_2021/Task21.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AoC_2021
+{
+    [TestFixture]
+    public class Task21
+    {
+        [Test]
+        [TestCaseSource(nameof(GenerateTestCases))]
+        public void Solve(int start1, int start2, int dieCount, long expected)
+        {
+            var playerPositions = new[] { start1, start2 };
+            var totalScores = new[] { 0L, 0L };
+            var playerIndex = 0;
+
+            var die = 0;
+            var totalDie = 0L;
+
+            while (true)
+            {
+                var step = die % dieCount + 1 +
+                           (die + 1) % dieCount + 1 +
+                           (die + 2) % dieCount + 1;
+
+                var currentScore = (playerPositions[playerIndex] - 1 + step) % 10 + 1;
+                playerPositions[playerIndex] = currentScore;
+                totalScores[playerIndex] += currentScore;
+
+                totalDie += 3;
+                die = (die + 3) % dieCount;
+                playerIndex = (playerIndex + 1) % 2;
+
+                if (totalScores.Any(x => x >= 1000))
+                {
+                    (totalScores[playerIndex] * totalDie).Should().Be(expected);
+                    return;
+                }
+            }
+        }
+
+        private static IEnumerable<TestCaseData> GenerateTestCases()
+        {
+            yield return new TestCaseData(4, 8, 100, 739785);
+            yield return new TestCaseData(7, 5, 100, 798147);
+        }
+    }
+}
---
+++ AoC_2021/Task21_2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AoC_2021
+{
+    [TestFixture]
+    public class Task21_2
+    {
+        [Test]
+        [TestCaseSource(nameof(GenerateTestCases))]
+        public void Solve(int start1, int start2, int winScore, long expected)
+        {
+            var game = new Game
+            {
+                Player1Position = start1,
+                Player2Position = start2,
+                Player1Score = 0,
+                Player2Score = 0,
+                NextTurnIsPlayer1 = true
+            };
+            
+            var wins = Turn(game, winScore);
+            var max = Math.Max(wins.Win1, wins.Win2);
+            max.Should().Be(expected);
+        }
+
+        private static (long Win1, long Win2) Turn(Game game, int winScore)
+        {
+            if (game.Player1Score >= winScore)
+            {
+                return (1, 0);
+            }
+
+            if (game.Player2Score >= winScore)
+            {
+                return (0, 1);
+            }
+
+            var turns = new List<(long Win1, long Win2)>(3);
+            for (var die = 1; die <= 3; ++die)
+            {
+                var newGame = game;
+                newGame.NextTurnIsPlayer1 = !newGame.NextTurnIsPlayer1;
+                if (game.NextTurnIsPlayer1)
+                {
+                    newGame.Player1Position = (game.Player1Position - 1 + die) % 10 + 1;
+                    newGame.Player1Score += newGame.Player1Position;
+                }
+                else
+                {
+                    newGame.Player2Position = (game.Player2Position - 1 + die) % 10 + 1;
+                    newGame.Player2Score += newGame.Player2Position;
+                }
+
+                var turn = Turn(newGame, winScore);
+                turns.Add(turn);
+            }
+
+            return (turns.Select(x => x.Win1).Sum(), turns.Select(x => x.Win2).Sum());
+        }
+
+        struct Game
+        {
+            public int Player1Position;
+            public int Player2Position;
+            public long Player1Score;
+            public long Player2Score;
+            public bool NextTurnIsPlayer1;
+        }
+
+        private static IEnumerable<TestCaseData> GenerateTestCases()
+        {
+            yield return new TestCaseData(0, 0, 2, 5L);
+            yield return new TestCaseData(0, 0, 3, 14L);
+            yield return new TestCaseData(4, 8, 21, 444356092776315L);
+            yield return new TestCaseData(7, 5, 21, 0L);
+        }
+    }
+}
